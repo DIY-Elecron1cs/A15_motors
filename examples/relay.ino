d7bc8e53d6220e8d6902e4e882b43dde852f0484
DIY электроника <private.property240702@gmail.com>
2024-12-20 18:08:03 +0300

    Update relay.ino
    Signed-off-by: DIY электроника <private.property240702@gmail.com>

--- examples/relay.ino
+++ examples/relay.ino
@@ -1,10 +1,14 @@
 #include <A15Motors.h>
 
-A15Relay relay(13);
+A15Relay relay1(13);
+A15Relay relay2(0);
+A15Relay relay3(1);
 
 void setup(){
 }
 
 void loop(){
-  work(200);
+  relay1.work(200);
+  relay2.work(500);
+  relay3.work(1000);
 }
