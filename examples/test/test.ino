44522e7fc5e6b2b0ab335bf56a1c7d17d63197f6
DIY электроника <private.property240702@gmail.com>
2024-12-21 11:04:21 +0300

    Create test.ino
    Signed-off-by: DIY электроника <private.property240702@gmail.com>

---
+++ examples/test/test.ino
@@ -0,0 +1,17 @@
+#include <A15Motors.h>
+
+A15Motors motors(6, 9, 10, 11);
+
+void setup(){
+  motors.setSpeed(180);
+  motors.setSmooth(5);
+}
+
+void loop(){
+  for(int i = 0; i < 4; i++){
+    motors.smoothGo(FW);
+    motors.smoothBrake(FW);
+    motors.smoothGo(L);
+    motors.smoothBrake(L);
+  }
+}
